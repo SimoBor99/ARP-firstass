3dc62df808be1ed3d70e01ad4430899c92d40a95
SimoBor99 <100366282+SimoBor99@users.noreply.github.com>
2022-11-10 23:00:27 +0100

    Update mx.c

--- mx.c
+++ mx.c
@@ -23,11 +23,11 @@ void comunication_channel_world ( char* myfifo_world, double posx, int fd1) {
 }
 
 //function for opening comunication channel to comand
-double comunication_channel_comand ( char* myfifo_comand, int fd2) {
+double comunication_channel_comandx ( char* myfifo_comandx, int fd2) {
     char velx_str[20]="";
     char format_string[20]="%f";
     double velx=0.0;
-    mkfifo(myfifo_comand, 0666);
+    mkfifo(myfifo_comandx, 0666);
     if (!read(fd2, velx_str, strlen(velx_str)+1))
         perror("Something wrong in reading");
     else 
@@ -39,7 +39,7 @@ int main() {
     //open comunication channel to world process
      char * myfifo_world = "/tmp/myfifo_world";
      //open comunication channel to comand process
-     char * myfifo_comand = "/tmp/myfifo_comand";
+     char * myfifo_comandx = "/tmp/myfifo_comandx";
      char input[20]="";
      double posx=0.0;
      double velx=0.0;
@@ -48,7 +48,7 @@ int main() {
      while(1) {
         fd1=open(myfifo_world,O_WRONLY);
         fd2=open(myfifo_comand,O_RDONLY);
-        velx=comunication_channel_comand(myfifo_comand, fd2);
+        velx=comunication_channel_comandx(myfifo_comandx, fd2);
         posx=estimate_position(velx, delta_time);
         //TODO check if we are 0 or 100, we cannot accept, respectly, negative or positive velx
         comunication_channel_world(myfifo_world, posx, fd1);
