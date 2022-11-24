e43b3407ff54259222e6a9a6e3efee2358d57591
SimoBor99 <100366282+SimoBor99@users.noreply.github.com>
2022-11-24 16:45:02 +0100

    Update master.c

--- src/master.c
+++ src/master.c
@@ -8,11 +8,7 @@
 #include <math.h>
 #include<signal.h>
 
-FILE *fp1;
-FILE *fp2;
-FILE *fp3;
-FILE *fp4;
-FILE *fp5;
+FILE *fp[5];
 //function that read the log file of our porcesses
 char* readlogfile(char *filepath) {
     struct stat filestat;
@@ -33,12 +29,12 @@ char* readlogfile(char *filepath) {
     return time;
 }
 //check if proces is idle more than 60 seconds
-int timecheck( char* filetime) {
+int timecheck( char *filetime) {
   time_t rawtime;
   struct tm * timeinfo;
   char format_string_time[100]="%d, %d, %d";
   if ( timeinfo==NULL || format_string_time==NULL)
-    return -1;
+    perror("Invalid pointer");
   char actual_time[100];
   int actual_hour=0;
   int actual_minutes=0;
@@ -49,10 +45,11 @@ int timecheck( char* filetime) {
   time(&rawtime);
   timeinfo=localtime( &rawtime);
   //actual time
-  strftime(actual_time, 100, " %H:%M:%S", timeinfo);
+  strftime(actual_time, 100, " %H %M %S", timeinfo);
   //convertion of actual and last modify file time
   sprintf(actual_time, format_string_time, &actual_hour, &actual_minutes, &actual_seconds);
   sprintf(filetime, format_string_time, &hours, &minutes, &seconds);
+  //check if 1 minute has gone
   if ( abs(actual_minutes-minutes)>=1 && abs(actual_seconds-seconds)>=0 || abs(actual_minutes-minutes)>=2) 
     return 1;
   return 0;
@@ -78,7 +75,7 @@ int spawn(const char * program, char * arg_list[]) {
     return 1;
   }
 }
-//function that pass filename and pid to processes
+//function that pass our file name and pid to processes
 int send_pid_file ( const char * program, int pid, char *filename) {
     char pids[100];
     sprintf(pids,"%d", pid);
@@ -89,59 +86,49 @@ int send_pid_file ( const char * program, int pid, char *filename) {
     return 0;
 }
 
-int main() {
-
-  char * arg_list_command[] = { "/usr/bin/konsole", "-e", "./bin/command", NULL };
-  char * arg_list_inspection[] = { "/usr/bin/konsole", "-e", "./bin/inspection", NULL };
-  char * arg_list_mx[] = { "/usr/bin/mx", "-e", "./bin/mx", NULL };
-  char * arg_list_mz[] = { "/usr/bin/mz", "-e", "./bin/mz", NULL };
-  char * arg_list_world[] = { "/usr/bin/world", "-e", "./bin/world", NULL };
-  pid_t pid_cmd = spawn("/usr/bin/konsole", arg_list_command);
-  pid_t pid_insp = spawn("/usr/bin/konsole", arg_list_inspection);
-  pid_t pid_mx=spawn("/usr/bin/mx", arg_list_mx);
-  pid_t pid_mz=spawn("/usr/bin/mz", arg_list_mz);
-  pid_t pid_world=spawn("/usr/bin/world", arg_list_world);
-  //set file names
-  char *file_name[]={"log_command.txt", "log_inspection.txt", "log_mx.txt", "log_mz.txt", "log_world.txt"};
-  //serial calls of send_pid_file
-  send_pid_file ( arg_list_command[2], pid_cmd, file_name[0]);
-  send_pid_file ( arg_list_inspection[2], pid_insp, file_name[1]);
-  send_pid_file ( arg_list_mx[2], pid_mx, file_name[2]);
-  send_pid_file ( arg_list_mz[2], pid_mz, file_name[3]);
-  send_pid_file ( arg_list_world[2], pid_world, file_name[4]);
-  char filemod[5]={0};
-  int status;
-  if (filemod==NULL)
+int kill_processes( int pid, int status) {
+  if (kill(pid, SIGINT)==-1) {
+    perror("Something went wrong in kill");
     return -1;
+  }
+  if (waitpid(pid, &status, 0)==-1) {
+    perror("Error in waitpid");
+    return -1;
+  }
+  return 0;
+}
+
+int main() {
+  //list of all processes
+  char *arg_lists[][100]= {{"/usr/bin/konsole", "-e", "./bin/command", NULL}, {"/usr/bin/konsole", "-e", "./bin/inspection", NULL}, 
+  {"./bin/mx", NULL }, {"./bin/mz", NULL }, {"./bin/world", NULL }};
+  //list of processes path
+  char *process_path[][100]={{"./bin/command"}, {"./bin/inspection"}, {"./bin/mx"}, {"./bin/mz"}, {"./bin/world"}};
+  //array that collects all pid
+  int all_pid[5];
+  for ( int i=0; i<5; i++)
+    all_pid[i]=spawn(*process_path[i], arg_lists[i]);
+  //set file names
+  char *file_name[][100]={"log_command.txt", "log_inspection.txt", "log_mx.txt", "log_mz.txt", "log_world.txt"};
+  for ( int i=0; i<5; i++)
+    send_pid_file(*process_path[i], all_pid[0], *file_name[i]);
+  char *time_filemod[5]={0};
+  int status;
   while (1) {
-    fp1=fopen("log_command.txt", "r");
-    fp2=fopen("log_inspection.txt", "r");
-    fp3=fopen("log_mx.txt", "r");
-    fp4=fopen("log_mz.txt", "r");
-    fp5=fopen("log_world.txt", "r");
-    if (fp1==NULL || fp2==NULL || fp3==NULL || fp4==NULL || fp5==NULL) 
+    for (int i=0; i<5; i++)
+      fp[i]=fopen(*file_name[i], "r");
+    if (fp[0]==NULL || fp[1]==NULL || fp[2]==NULL || fp[3]==NULL || fp[4]==NULL) 
         perror("Error in read files");
-    filemod[0]=readlogfile(file_name[0]);
-    filemod[1]=readlogfile(file_name[1]);
-    filemod[2]=readlogfile(file_name[2]);
-    filemod[3]=readlogfile(file_name[3]);
-    filemod[4]=readlogfile(file_name[4]);
+    for ( int i=0; i<5; i++)
+      time_filemod[i]=readlogfile(*file_name[i]);
     //check if all processes are idle more than 60 seconds
-    if (timecheck(filemod[0])==1 && timecheck(filemod[1])==1 && timecheck(filemod[2])==1 && timecheck(filemod[3])==1 && timecheck(filemod[4])==1) {
+    if (timecheck(time_filemod[0])==1 && timecheck(time_filemod[1])==1 && timecheck(time_filemod[2])==1 && timecheck(time_filemod[3])==1 && timecheck(time_filemod[4])==1) {
       //kill all child processes
-      kill(pid_cmd, SIGINT);
-      waitpid(pid_cmd, &status, 0);
-      kill(pid_insp, SIGINT);
-      waitpid(pid_insp, &status, 0);
-      kill(pid_mx, SIGINT);
-      waitpid(pid_mx, &status, 0);
-      kill(pid_mz, SIGINT);
-      waitpid(pid_mz, &status, 0);
-      kill(pid_world, SIGINT);
-      waitpid(pid_world, &status, 0) ;
+      for(int i=0; i<5; i++)
+        kill_processes(all_pid[i], status);
       //process kill itself
-      fcloseall();
-      exit(1);
+    fcloseall();
+    exit(1);
     }
     fcloseall();
   }
