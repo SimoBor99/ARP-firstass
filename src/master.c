2c19ea073cdde9ef32bf232d20309b407c319ddc
SimoBor99 <100366282+SimoBor99@users.noreply.github.com>
2022-12-12 22:19:37 +0100

    Update master.c

--- src/master.c
+++ src/master.c
@@ -8,112 +8,68 @@
 #include <math.h>
 #include <signal.h>
 #include <string.h>	
-#include <fcntl.h> 
+#include <fcntl.h>
 
+const int dim=100;
 FILE *fp[5];
-//function that read the log file of our porcesses
+//function that read the log file of our processes
+
+const char* readlogfile(char *filepath) {
+    struct stat filestat;
+    char day[dim];
+    char month[dim];
+    char time[dim];
+    char num_day[dim];
+    char year[dim];
+    if (stat(filepath,&filestat)==-1)
+        perror("Something went wrong");
+    char *filemod=ctime(&filestat.st_mtime);
+    sscanf(filemod, "%s %s  %s %s %s", day, month, num_day, time, year);
+    const char const *ptime=&time[0];
+    //printf("%s\n", ptime);
+    return ptime;
+}
 
 void write_log(char * log_text, char * fn)
 {
 	FILE *fp_log;
-	fp_log = fopen(fn,"a");  
+	fp_log = fopen(fn,"a");
+  if (fp_log==NULL)
+    perror("Error in open file");  
 	fputs(log_text, fp_log);
 	fputs("\n", fp_log);
 	//perror("Error in something!"); 
 	fclose(fp_log);
 }
 
-char* readlogfile(char *filepath, int k) {
-    struct stat filestat;
-    char * filemod;
-    if (filemod==NULL) 
-      return -1;
-    char format_string_date[100]="%s, %s, %s, %s, %s";
-    char day[80];
-    char month[80];
-    char num_day[80];
-    char year[80];
-    if (stat(filepath,&filestat)==-1)
-        perror("Something went wrong");
-    filemod=ctime(&filestat.st_mtime);
-    int i = 0;
-    char * time = strtok(filemod, " ");
-    while (i < 3) {
-    	time = strtok(NULL, " ");
-    	i++;
-    }
-    //time of last file modify
-    char * try = time;
-    //write_log(str, "master.txt");
-    //write_log(try, "master.txt");
-    return try;
-}
-//check if proces is idle more than 60 seconds
-int timecheck( char *filetime, char * minutes, char * seconds, int j) {
-    char * str = "";
-    if (j == 0) {
-    	str = "COMMAND: ";
-    }
-    if (j == 1) {
-    	str = "INSPECTION: ";
-    }
-    if (j == 2) {
-    	str = "MX: ";
-    }
-    if (j == 3) {
-    	str = "MZ: ";
-    }
-    if (j == 4) {
-    	str = "WORLD: ";
-    }
-    write_log("FUN TIME: ","master.txt");
-    write_log(str,"master.txt");
-    write_log(minutes,"master.txt");
-    write_log(seconds,"master.txt");
-    write_log("\n","master.txt");
+int timecheck( const char const *filetime) {
   time_t rawtime;
-  struct tm * timeinfo;
-  char format_string_time[100]="%d, %d, %d";
-  if ( timeinfo==NULL || format_string_time==NULL)
-    perror("Invalid pointer");
-  char actual_time[100];
-  char * pnt = &actual_time[0];
-  char * at;
-  int actual_minutes=0;
-  int actual_seconds=0;
+  char current_time[dim];
+  char file_time[dim];
+  int current_hour=0;
+  int current_minutes=0;
+  int current_seconds=0;
   int file_minutes=0;
   int file_seconds=0;
+  int file_hour=0;
+  //printf("D: %s\n", filetime);
   time(&rawtime);
-  timeinfo=localtime( &rawtime);
+  struct tm *timeinfo=localtime( &rawtime);
   //actual time
-  strftime(actual_time, 100, "%H %M %S", timeinfo);
-  //convertion of actual and last modify file time
-  char * h = strtok(pnt," ");
-  char * m = strtok(NULL," ");
-  char * s = strtok(NULL," ");
-  //write_log("ADESSO MINUTO: ","master.txt");
-  //write_log(m,"master.txt");
-  //write_log("ADESSO SECONDO: ","master.txt");
-  //write_log(s,"master.txt");
-  //write_log("\n","master.txt");
-  actual_minutes = atoi(m);
-  actual_seconds = atoi(s);
-  file_minutes = atoi(minutes);
-  file_seconds = atoi(seconds);
-  //check if 1 minute has gone
-  if (actual_minutes != file_minutes) {
-  	if (actual_minutes == 0) {
-  		actual_minutes = 60;
+  strftime(current_time, dim, "%H:%M:%S", timeinfo);
+  //convertion of current and last modify file time
+  memcpy(file_time, filetime, sizeof(filetime));
+  sscanf(current_time, "%d:%d:%d", &current_hour, &current_minutes, &current_seconds);
+  sscanf(file_time, "%d:%d:%d", &file_hour, &file_minutes, &file_seconds);
+  if (current_minutes != file_minutes) {
+  	if (current_minutes == 0) {
+  		current_minutes = 60;
   	}
-  	if (abs(actual_minutes-file_minutes) > 1) {
-  		write_log(str, "master.txt");
-    		write_log("2 O PIU MINUTI", "master.txt");
+  	if (abs(current_minutes-file_minutes)>1) {
   		return 1;
   	}
   	else {
-  		if (actual_seconds - file_seconds >= 0) {
-  			write_log(str, "master.txt");
-    			write_log("1 MINUTO E PIU SECONDI", "master.txt");
+  		if ((current_seconds-file_seconds)>= 0) {
   			return 1;
   		}
   		else {
@@ -124,32 +80,6 @@ int timecheck( char *filetime, char * minutes, char * seconds, int j) {
   else {
   	return 0;
   }
-}
-
-// NUOVE FUNZIONI PER ESTRARRE MINUTI E ORE
-
-char* ext_min(char* times) {
-	int i = 0;
-	char * min = strtok(times, ":");
-	while (i < 1) {
-		min = strtok(NULL, ":");
-    		i++;
-	}
-	//write_log("FUN","master.txt");
-        //write_log(min,"master.txt");
-	return min;
-}
-
-char* ext_sec(char* times) {
-	int i = 0;
-	char * sec = strtok(times, ":");
-	while (i < 2) {
-		sec = strtok(NULL, ":");
-    		i++;
-	}
-	//write_log("FUN SEC","master.txt");
-        //write_log(sec,"master.txt");
-	return sec;
 }
 
 // FINE NUOVE FUNZIONI
@@ -175,7 +105,7 @@ int spawn(const char * program, char * arg_list[]) {
 }
 
 int kill_processes( int pid, int status) {
-  if (kill(pid, SIGINT)==-1) {
+  if (kill(pid, SIGTERM)==-1) {
     perror("Something went wrong in kill");
     return -1;
   }
@@ -185,16 +115,44 @@ int kill_processes( int pid, int status) {
   }
   return 0;
 }
+//some functions that I have to implement later
+/*void open_file(const char** const file_name) {
+      for ( int i=0; i<5; i++) {
+        fp[i]=fopen(*file_name[i], "r");
+        if (fp[i]==NULL)
+          perror("Somethin went wrong in opening file");
+      }
+}
+
+void close_file(const char** const file_name, FILE **fp) {
+  for ( int i=0; i<5; i++)
+    fclose(fp[i]);
+}*/
+
+int check_pipe( char *myfifo) {
+  if (mkfifo(myfifo, 0666)!=0) {
+    perror("Cannot open fifo. Already exists?");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
+}
 
 int main() {
-
-  //BLOCCO DI CODICE TEMPORANEO
-  
+  int fd;
+  char write_pidmx[dim];
+  char write_pidmz[dim];
+  char *myfifo[][100]={{"/tmp/myfifo_insp"}, {"/tmp/myfifo_inspx"}, {"/tmp/myfifo_inspz"}, {"/tmp/myfifo_comandx"}, {"/tmp/myfifo_comandz"}, {"/tmp/myfifo_worldz"}, {"/tmp/myfifo_worldx"}};
   char *file_name[][100]={{"log_command.txt"}, {"log_inspection.txt"}, {"log_mx.txt"}, {"log_mz.txt"}, {"log_world.txt"}};
+  //list of all processes
+  char *arg_lists[][100]= {{"/usr/bin/konsole", "-e", "./command", *file_name[0], *myfifo[3], *myfifo[4], NULL}, {"/usr/bin/konsole", "-e", "./inspection", *file_name[1], *myfifo[1], *myfifo[2], *myfifo[0], NULL}, 
+  {"./mx", *file_name[2], *myfifo[3], *myfifo[6],  NULL }, {"./mz", *file_name[3], *myfifo[4], *myfifo[5], NULL }, {"./world", *file_name[4], *myfifo[1], *myfifo[2], *myfifo[5], *myfifo[6], NULL }};
+  //list of processes path
+  char *process_path[][100]={{"/usr/bin/konsole"}, {"/usr/bin/konsole"}, {"./mx"}, {"./mz"}, {"./world"}};
+  //array that collects all pid
   for (int i=0; i<5; i++)
   {
       while(1) {
-      	fp[i]=fopen(*file_name[i], "w+");
+      	fp[i]=fopen(*file_name[i], "a");
         if(fp[i] == NULL) {
         	//printf("CREA PADRE?");
         }
@@ -206,86 +164,57 @@ int main() {
      }
      //printf("%d",i);
   }
-  char* myfifo_insp="/tmp/myfifo_insp";
-  mkfifo(myfifo_insp, 0666);
-  //FINE BLOCCO DI CODICE TEMPORANEO
-  int fd;
-  //INIZIALIZZAZIONI NUOVE
-  char * sec[5];
-  char * min[5];
-  //FINE INIZIALIZZAZIONI NUOVE
-  char write_pidmx[100];
-  char write_pidmz[100];
-  //list of all processes
-  char *arg_lists[][100]= {{"/usr/bin/konsole", "-e", "./command", *file_name[0], NULL}, {"/usr/bin/konsole", "-e", "./inspection", *file_name[1], NULL}, 
-  {"/usr/bin/konsole", "-e", "./mx", *file_name[2], NULL }, {"/usr/bin/konsole", "-e", "./mz", *file_name[3], NULL }, {"/usr/bin/konsole", "-e", "./world", *file_name[4], NULL }};
-  //list of processes path
-  char *process_path[][100]={{"/usr/bin/konsole"}, {"/usr/bin/konsole"}, {"/usr/bin/konsole"}, {"/usr/bin/konsole"}, {"/usr/bin/konsole"}};
-  //array that collects all pid
+  for ( int i=0; i<7; i++)
+    check_pipe(*myfifo[i]);
   int all_pid[5];
-  int cool[5];
   for ( int i=0; i<5; i++)
     all_pid[i]=spawn(*process_path[i], arg_lists[i]);
-  char * time_filemod[5];
-  if ( time_filemod==NULL)
-  	perror("ssss");
-  int status;
+  fd=open(*myfifo[0], O_WRONLY);
+  if (fd==0) {
+    perror("Cannot open fifo");
+    unlink(*myfifo[0]);
+    exit(1);
+  }
   sprintf(write_pidmx, "%d", all_pid[2]);
   sprintf(write_pidmz, "%d", all_pid[3]);
-  write_log(write_pidmx, "inspection_log.txt");
-  write_log(write_pidmz, "inspection_log.txt");
-  strcat(write_pidmx, "-");
+  strcat(write_pidmx, ",");
   strcat(write_pidmx, write_pidmz);
-  strcat(write_pidmx, "-");
-  fd=open(myfifo_insp, O_WRONLY);
-  if (fd==0)
-    perror("invalid pipe");
-  if (write(fd, write_pidmx, 100)==-1)
+  if (write(fd, write_pidmx, dim)==-1)
     perror("Something went wrong");
   write_log(write_pidmx, "inspection_log.txt");
-  close(fd);
+  int status;
+  const char * time_filemod[5];
   while (1) {
-    //usleep(30000);
-    for (int i=0; i<5; i++)
-      fp[i]=fopen(*file_name[i], "r");
-    if (fp[0]==NULL || fp[1]==NULL || fp[2]==NULL || fp[3]==NULL || fp[4]==NULL) 
-        perror("Error in read files");
-    for ( int i=0; i<5; i++) {
-      char tmp[10];
-      time_filemod[i]=readlogfile(*file_name[i],i);
-      strcpy(tmp,time_filemod[i]);
-      min[i] = ext_min(tmp);
-      sec[i] = ext_sec(time_filemod[i]);
-      char tmp1[80];
-      char tmp2[80];
-      strcpy(tmp1, min[i]);
-      strcpy(tmp2, sec[i]);
-      write_log("MAIN MINUTO: ","master.txt");
-      write_log(tmp1,"master.txt");
-      write_log("MAIN SECONDO: ","master.txt");
-      write_log(tmp2,"master.txt");
-      write_log("\n","master.txt");
-      cool[i] = timecheck(time_filemod[i],tmp1,tmp2,i);
-     }
+    for (int i=0; i<5; i++) {
+    fp[i]=fopen(*file_name[i], "r");
+      if(fp[i] == NULL) {
+        perror("Error in opening file!");
+      }
+  }
+    for ( int i=0; i<5; i++)
+      time_filemod[i]=readlogfile(*file_name[i]);
     //check if all processes are idle more than 60 seconds
-     if (cool[0] + cool[1] + cool[2] + cool[3] + cool[4] == 5) {
+    if (timecheck(time_filemod[0])==1 && timecheck(time_filemod[1])==1 && timecheck(time_filemod[2])==1 && timecheck(time_filemod[3])==1 && timecheck(time_filemod[4])==1) {
       //kill all child processes
       for(int i=0; i<5; i++) {
         sleep(0.5);
         kill_processes(all_pid[i], status);
         }
       //process kill itself
-      kill_processes(all_pid[0], status);
-    for (int i = 0; i < 5; i++) {
-    	fclose(fp[i]);
+      //
+      for (int i = 0; i < 5; i++)
+    	  fclose(fp[i]);
+      close(fd);
+      for ( int i=0; i<7; i++)
+        unlink(*myfifo[i]);
+      printf ("Main program exiting with status %d\n", status);
+      exit(1);
     }
-    printf ("Main program exiting with status %d\n", status);
-    exit(1);
-    } 
-    for (int i = 0; i < 5; i++) {
-    	fclose(fp[i]);
-    }
+    for ( int i=0; i<5; i++)
+      fclose(fp[i]);
+
   }
-  printf ("Main program exiting with status %d\n", status);
+  /*for ( int i=0; i<7; i++)
+        unlink(*myfifo[i]);*/
   return 0;
-} 
+}
